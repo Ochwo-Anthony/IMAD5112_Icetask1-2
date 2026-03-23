433f8a404adc5e84128c45f54076ca8200567acb
ST10395938 <130601150+Ochwo-Anthony@users.noreply.github.com>
2026-03-23 10:54:39 +0200

    First Commit
    Ice Task 1 and 2

---
+++ app/src/main/java/com/example/registerandlogin/WelcomePage.kt
@@ -0,0 +1,30 @@
+package com.example.registerandlogin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WelcomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome_page)
+
+        val launchButton = findViewById<Button>(R.id.btnLaunch)
+
+        launchButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
